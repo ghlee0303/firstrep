e0e4e7ece253f219986902efaccc7ba8501806ad
ghlee <rjs993530@gmail.com>
2022-02-07 09:57:55 +0900

    202202070957

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 
-const app = express(); 
+const app = express();
+const { sequelize } = require('./models'); // db.sequelize
+
+sequelize.sync({ force: false })
+    .then(() => {
+        console.log('데이터베이스 연결됨.');
+    }).catch((err) => {
+        console.error(err);
+    });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.send("Hello World");
